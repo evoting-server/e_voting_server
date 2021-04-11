5991a55132fd3f1fc7bd68b39d79f292a9bd89de
evoting-server <81847670+evoting-server@users.noreply.github.com>
2021-04-11 15:19:47 +0300

    Create uuid.js

---
+++ src/uuid.js
@@ -0,0 +1,14 @@
+function uuid(length) {
+  const result = [];
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result.push(
+      characters.charAt(Math.floor(Math.random() * charactersLength))
+    );
+  }
+  return result.join("");
+}
+
+exports.uuid = uuid;
